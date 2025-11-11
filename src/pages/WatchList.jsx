9991b107548b71bdcdec3b5f2b9f715a2791e440
SmitaraneeSahu <74060140+SmitaraneeSahu@users.noreply.github.com>
2025-11-11 21:35:48 +0530

    Update WatchList.jsx

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
-export default function WatchList() {
+import Card from '../components/Card'
+export default function WatchList({darkMode}) {
   
-const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
+// const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
 function removeFromWatchlist(movieId) {
   const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
   const updated = watchlist.filter(item => item.id !== movieId);
@@ -13,12 +13,17 @@ return (
   <main>
   <div>
     <h2>Your Watchlist</h2>
-    {watchlist.map(movie => (
+    /* {watchlist.map(movie => (
       <div key={movie.id}>
         <h3>{movie.title}</h3>
         <button onClick={() => removeFromWatchlist(movie.id)}>Remove</button>
       </div>
-    ))}
+    ))} */
+    <div className="flex  flex-wrap justify-center m-auto">
+        {watchlist.map((movie)=>(
+          <Card key={movie.id} home={movie} darkMode={darkMode}/>
+        ))}
+      </div>
   </div>
   </main>
 );
